1556d2b0483518bdde638ff5992e77430c7f056d
Dave <daves125125@gmail.com>
2017-10-24 22:47:39 +0100

    Split TPR config and only apply when registration complete

--- manifests/monitoring/prometheus/apply.sh
+++ manifests/monitoring/prometheus/apply.sh
@@ -3,5 +3,8 @@
 cd $(dirname $0)
 
 kubectl apply -f operator.yml
-kubectl apply -f prometheus.yml
 kubectl apply -f service.yml
+
+# Wait until the TPR (third party resource) for Prometheus is registered before applying dependent manifests
+until kubectl get prometheus; do sleep 1; done
+kubectl apply -f prometheus.yml
--- manifests/services/sample/apply.sh
+++ manifests/services/sample/apply.sh
@@ -3,3 +3,7 @@
 cd $(dirname $0)
 
 kubectl apply -f sample.yml
+
+# Wait until the TPR (third party resource) for Prometheus is registered before applying dependent manifests
+until kubectl get prometheus; do sleep 1; done
+kubectl apply -f service-monitor.yml
